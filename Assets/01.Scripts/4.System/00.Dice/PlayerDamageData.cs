5a2181b2aaf096771132331a5f3a8c670d7b045d
wallyyoo <hyase22@naver.com>
2025-06-25 20:26:19 +0900

    add : 플레이어 데미지 계산
    플레이어 데미지 계산 스크립트 생성

---
+++ Assets/01.Scripts/4.System/00.Dice/PlayerDamageData.cs
@@ -0,0 +1,15 @@
+
+public class PlayerDamageData
+{
+    public int finalDamage;     //최종 데미지
+    public float redMultiplier; //빨간색 주사위 : 데미지 증가
+    public float counterDamageReduction;    //파란색 주사위 : 받는 데미지감소
+    public float stunChance;    //검은색 주사위 : 적 스턴 확률
+    public int nextTurnExtraReroll; //노란색 주사위 : 다음턴 리롤 횟수 증가
+
+    public override string ToString()
+    {
+        return $"[Damage: {finalDamage}] [Red Mult: {redMultiplier}] [Counter↓: {counterDamageReduction}] [Stun: {stunChance}] [Extra Reroll: {nextTurnExtraReroll}]";
+    }
+}
+// playerDamageData = damageCalculator.GetPlayerDamageData(); 으로 데미지 가져오기
